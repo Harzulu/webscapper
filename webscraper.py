598d971ce662b651b56839d8f94042cfff6e1a5d
Nathan Jesudason <nathan.jesudason@gmail.com>
2018-10-01 16:59:28 -0700

    Add URL checker

--- webscraper.py
+++ webscraper.py
@@ -5,6 +5,19 @@ def WebData():
 	
 	link = raw_input ("Paste URL here:")
 	sec = int(raw_input("How many seconds do you want to test for?"))
+	try:
+    		webUrl = urllib2.urlopen(link)
+   	if(webUrl.getcode() == 200):
+        	print("URL acceptable")
+        	link = '%s' %link
+    	else:
+        	code = webUrl.getcode()
+        	print("Error, web code return was '%s'" % code)
+        	print("Using Nathan's Unity Blog")
+        	link = 'https://nathan440.wordpress.com/'
+	except Exception:
+   		print("Request either nothing or not understand. Using Nathan's Unity Blog")
+    		link = 'https://nathan440.wordpress.com/'
 	
 	URL = urllib2.urlopen(link)
 	data = URL.read()
